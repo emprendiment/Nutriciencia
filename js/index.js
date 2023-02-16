475ad9573f503c41d2f089a94a0127586f26e641
Mendoza Matias <mendoza.matias.ezequiel.01@gmail.com>
2023-02-16 12:24:03 -0300

    Realizacion de la navegacion

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+/**Cuando hago click en la clase button le añado y le quito la clase active (toogle)
+ * toogle de la clase active
+*/
+
+const button = document.querySelector('.button');
+
+const nav = document.querySelector('.nav');
+
+button.addEventListener('click',()=>{
+    nav.classList.toggle('activo')
+});
